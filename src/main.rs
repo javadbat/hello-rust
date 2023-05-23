4a1b1cb6bbdd9f3f4ff566c3c248e7bb76eb5dfc
javadbat <javadbat@gmail.com>
2023-05-23 19:26:35 +0330

    start ch6

--- src/main.rs
+++ src/main.rs
@@ -62,31 +62,3 @@ async fn run_server(port: u16) {
         .recover(return_error);
     warp::serve(routes).run(([127, 0, 0, 1], port)).await;
 }
-// async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-//     if let Some(error) = r.find::<PaginationError>() {
-//         Ok(warp::reply::with_status(
-//             error.to_string(),
-//             StatusCode::RANGE_NOT_SATISFIABLE,
-//         ))
-//     } else if let Some(error) = r.find::<QuestionError>() {
-//         Ok(warp::reply::with_status(
-//             error.to_string(),
-//             StatusCode::NOT_FOUND,
-//         ))
-//     } else if let Some(error) = r.find::<CorsForbidden>() {
-//         Ok(warp::reply::with_status(
-//             error.to_string(),
-//             StatusCode::FORBIDDEN,
-//         ))
-//     } else if let Some(error) = r.find::<BodyDeserializeError>() {
-//         Ok(warp::reply::with_status(
-//             error.to_string(),
-//             StatusCode::UNPROCESSABLE_ENTITY,
-//         ))
-//     } else {
-//         Ok(warp::reply::with_status(
-//             "Route not found".to_string(),
-//             StatusCode::NOT_FOUND,
-//         ))
-//     }
-// }
